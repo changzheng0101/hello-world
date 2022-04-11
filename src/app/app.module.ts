6b9b070f49e543839b7aee015c67dacfdc6c0ac2
changzheng0101 <7589854+changzheng0101@user.noreply.gitee.com>
2022-04-11 20:31:35 +0800

    除删除外的方法实现

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,28 +1,29 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AuthorService } from './author.service';
+import { BootstrapPanelComponent } from './bootstrap-panel/bootstrap-panel.component';
+import { ChangePasswordComponent } from './change-password/change-password.component';
+import { ContactFormComponent } from './contact-form/contact-form.component';
+import { CourseFormComponent } from './course-form/course-form.component';
 import { CourseComponent } from './course/course.component';
 import { CoursesComponent } from './courses.component';
 import { CoursesService } from './courses.service';
+import { FormFormatDirective } from './form-format.directive';
+import { IfTestComponent } from './if-test/if-test.component';
+import { NewCourseFormComponent } from './new-course-form/new-course-form.component';
+import { PasswordFormComponent } from './password-form/password-form.component';
+import { PostsComponent } from './posts/posts.component';
+import { SignupFormComponent } from './signup-form/signup-form.component';
 import { StarButtonComponent } from './star-button/star-button.component';
 import { SummaryPipe } from './summary.pipe';
 import { TitleInputComponent } from './title-input/title-input.component';
 import { TitlePipe } from './title.pipe';
-import { BootstrapPanelComponent } from './bootstrap-panel/bootstrap-panel.component';
 import { TweetLikeComponent } from './tweet-like/tweet-like.component';
-import { IfTestComponent } from './if-test/if-test.component';
-import { FormFormatDirective } from './form-format.directive';
 import { ZippyComponent } from './zippy/zippy.component';
-import { ContactFormComponent } from './contact-form/contact-form.component';
-import { CourseFormComponent } from './course-form/course-form.component';
-import { SignupFormComponent } from './signup-form/signup-form.component';
-import { NewCourseFormComponent } from './new-course-form/new-course-form.component';
-import { PasswordFormComponent } from './password-form/password-form.component';
-import { ChangePasswordComponent } from './change-password/change-password.component';
-import { PostsComponent } from './posts/posts.component';
 
 
 @NgModule({
@@ -51,7 +52,8 @@ import { PostsComponent } from './posts/posts.component';
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    HttpClientModule
   ],
   providers: [
     CoursesService,
--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -1,15 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-posts',
   templateUrl: './posts.component.html',
   styleUrls: ['./posts.component.css']
 })
-export class PostsComponent implements OnInit {
+export class PostsComponent {
+  posts: any[] = [];
+  url: string = "https://jsonplaceholder.typicode.com/posts";
 
-  constructor() { }
+  constructor(private http: HttpClient) {
+    this.http.get(this.url)
+      .subscribe((result) => {
+        this.posts = result as any;
+        console.log(this.posts);
+      })
+  }
 
-  ngOnInit(): void {
+  createPost(inputTitle: HTMLInputElement) {
+    let post = { title: inputTitle.value };
+
+    this.http.post(this.url, post)
+      .subscribe((response) => {
+        this.posts.unshift(response);
+      })
+  }
+
+  updatePost(post: any) {
+    //patch 用于更新部分属性 post需要更新整个
+    this.http.patch(this.url + '/' + post.id, JSON.stringify({ isRead: true }))
+      .subscribe(response => {
+        console.log(response);
+      })
   }
 
 }
