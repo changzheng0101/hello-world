defa14feff28b1b1ae2e330a621624ffcf970050
changzheng0101 <7589854+changzheng0101@user.noreply.gitee.com>
2022-04-11 09:16:00 +0800

    form

--- src/app/contact-form/contact-form.component.ts
+++ src/app/contact-form/contact-form.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { NgModel } from '@angular/forms';
 
 @Component({
   selector: 'app-contact-form',
   templateUrl: './contact-form.component.html',
   styleUrls: ['./contact-form.component.css']
 })
-export class ContactFormComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+export class ContactFormComponent {
+  onChange(firstName: NgModel) {
+    console.log(firstName);
   }
+
 
 }
