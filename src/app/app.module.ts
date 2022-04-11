273ffbe1b6af4cee487ce5dc1b50577c6296d952
changzheng0101 <7589854+changzheng0101@user.noreply.gitee.com>
2022-04-11 14:02:35 +0800

    自定义验证

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -42,7 +42,8 @@ import { SignupFormComponent } from './signup-form/signup-form.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [
     CoursesService,
--- src/app/signup-form/signup-form.component.ts
+++ src/app/signup-form/signup-form.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { UserNameValidators } from './username.validators';
+
 
 @Component({
   selector: 'signup-form',
@@ -6,5 +9,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./signup-form.component.css']
 })
 export class SignupFormComponent {
-  
+  form = new FormGroup({
+    username: new FormControl('', [
+      Validators.required,
+      Validators.minLength(3),
+      UserNameValidators.CanNotContainSpace
+    ]),
+    password: new FormControl('', Validators.required)
+  })
+
+  get username(){
+    return this.form.get('username');
+  }
 }
