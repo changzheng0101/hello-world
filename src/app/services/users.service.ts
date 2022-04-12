563e285bd1db93843254515593a6b54d4ba0cb7a
changzheng0101 <7589854+changzheng0101@user.noreply.gitee.com>
2022-04-12 16:12:58 +0800

    添加heade和params

--- src/app/services/users.service.ts
+++ src/app/services/users.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from "@angular/common/http";
+import { HttpClient, HttpHeaders, HttpParams } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Observable } from "rxjs";
 import { environment } from "src/environments/environment";
@@ -16,7 +16,12 @@ export class UserService {
     }
 
     getUsers(): Observable<User[]> {
-        return this.http.get<User[]>(this.url);
+        let myHeaders = new HttpHeaders({ "myHeader": "myValue" });
+        myHeaders = myHeaders.set("test", "true"); //必须这么写 httpHeader是不可变的 直接设置没用
+
+        let myParams = new HttpParams({ fromString: 'name=cz&id=1' });
+        myParams = myParams.set("test", "true");
+        return this.http.get<User[]>(this.url, { headers: myHeaders, params: myParams });
     }
 
     getUser(): Observable<User> {
